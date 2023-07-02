c4f8075c230c40473763b3831439697a81eb744c
arfazhxss <arfazhussain@uvic.ca>
2023-07-01 19:24:53 -0700

    Routine Commit by @arfazhxss on Sat 01 Jul 2023

---
+++ 1 Fedora Builds/1 New Window/GLFW/main.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Window dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 1 //
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW initialisation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 2 //
+    // Setup GLFW window properties
+    // OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core Profile = No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow Forward Compatbility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 3 //
+    // Create the window
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!mainWindow)
+    {
+        printf("GLFW window creation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 4 //
+    // Get Buffer Size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 5 //
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW initialisation failed!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 6 //
+    // Setup Viewport size // x and y (top left and right), bufferWidth and bufferHeight (section in the middle).
+    // Size of the part, we are making this out entire view .. or smth????
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////// Part 7 //
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // Get + Handle user input events
+        glfwPollEvents(); // Check if any events have happened, resize click etc etc .. .. 
+
+        // Clear window // RGB Values not between 0-256 but 0-1
+        glClearColor(1.0f, 0.0f, 0.0f, 0.5f); // RED, GREEN, BLUE, ALPHA -->> (HOW TRANSPARENT IT IS)
+        glClear(GL_COLOR_BUFFER_BIT); // lots of things going, each pixel has color and depth data etc... we are clearing specific things
+
+        glfwSwapBuffers(mainWindow); // two windows, one being drawn and one we are seeing. this is the one we cant see and we are closing it
+    }
+
+    return 0;
+}
---
+++ 1 Fedora Builds/1 New Window/SDL2/main.cpp
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+
+void InitializeProgram()
+{
+}
+
+void MainLoop() {
+
+}
+
+void CleanUp() {
+
+}
+
+int main () {
+    initializeProgram();
+    MainLoop();
+    CleanUp();
+    return 0;
+}
