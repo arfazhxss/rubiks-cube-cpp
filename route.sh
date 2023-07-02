f7717e22ebd44fc0d282de2426eb0a70bbce4189
arfazhxss <arfazhussain@uvic.ca>
2023-07-01 20:24:23 -0700

    Bash Script Update Commit by @arfazhxss on Sat 01 Jul 2023

--- route.sh
+++ route.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-#auto-git v4.8
+#auto-git v5.0
 
 read -p "Do you want to fast forward and delete changes made after the last commit? (y/n): " answer
 
 if [[ $answer == "y" || $answer == "Y" ]]; then
-    git stash
-    git stash clear
-    git pull
-fi
-
-echo "Commit Message:"
-read commit_message
-
-if [[ -z "$commit_message" || ${#commit_message} -lt 3 ]]; then
-  commit_message="Routine Commit"
-  git add . && \
-  git add -u && \
-  git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-  git push origin HEAD
-  git log > commit-hist.txt
-  rm -rf .DS_Store/
+  git stash
+  git stash clear
+  git pull
 else
-  git add . && \
-  git add -u && \
-  git commit -m $"$commit_message"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-  git push origin HEAD
-  git log > commit-hist.txt
-  rm -rf .DS_Store/
+  echo "Commit Message:"
+  read commit_message
+
+  if [[ -z "$commit_message" || ${#commit_message} -lt 3 ]]; then
+    commit_message="Routine Commit"
+    git add . && \
+    git add -u && \
+    git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+    git push origin HEAD
+    git log > commit-hist.txt
+    rm -rf .DS_Store/
+  else
+    git add . && \
+    git add -u && \
+    git commit -m $"$commit_message"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+    git push origin HEAD
+    git log > commit-hist.txt
+    rm -rf .DS_Store/
+  fi
 fi
